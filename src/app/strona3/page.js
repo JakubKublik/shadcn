57807f763bd0e09b1c68f60e67a1d126366b0e87
JakubKublik <j.kublikjr@gmail.com>
2024-11-05 21:02:41 +0100

    1

--- src/app/strona3/page.js
+++ src/app/strona3/page.js
@@ -1,11 +1,21 @@
 "use client"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
 import PocketBase from 'pocketbase';
 import { useEffect, useState } from 'react';
 
 const pb = new PocketBase('http://172.16.15.142:8080');
+
 export default function Home(){
-    const[sklep, setSklep] = useState(null)
-    const[zdj,setZdj] = useState(null)
+    const[sklep,setSklep]=useState(null)
+    const[zdj,setZdj]=useState(null)
+    const[dane,setDane]=useState({nazwa: null, cena: null, opis:null})
     useEffect(()=>{
         const getData = async ()=>{
             try{
@@ -22,33 +32,76 @@ export default function Home(){
         }
         getData()
     },[])
+    const handleInputChange = (id,e)=>{
+        setDane((prev)=>({
+            ...prev,
+            [id]: e.target.value
+    }))
+    const handleSubmit = async ()=>{
+        const formData = new FormData()
+            formData.append("nazwa",dane.nazwa)
+            formData.append("cena",dane.cena)
+            formData.append("opis",dane.opis)
+            formData.append("zdj",zdj)
+        try{
+            const record = await pb.collection('sklep').create(formData);
+            setSklep((prev)=>([
+                ...prev,
+                record
+            ]))
+        }catch(err){
 
+        }
+    }
+    const handleZdj = (e)=>{
+        console.log(e)
+        setZdj(e.target.files[0])
+    }
+   
+    console.log(dane)
+    }
     return(
         <div>
-            {sklep &&
-                sklep.map((gry)=>(
-                    
-                    <Card key={gry} className="w-[400px] h-[400px]">
-            <CardTitle> <Image 
-                src={pb.files.getUrl(gry, gry.zdj)}
-                alt={gry.zdj}
-                width={500}
-                height={500}
-                /></CardTitle>
-            <CardDescription className="flex flex-wrap">{gry.nazwa}{gry.cena}</CardDescription>
-            <CardContent>
-            
-            </CardContent>
-            <CardFooter>
-
-
-            </CardFooter>
-        </Card>
+            <Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Dodaj gre</SheetTitle>
+      <SheetDescription>
+      {
+                sklep &&
                 
+                <div className='flex flex-wrap w-full justify-center gap-5'>
+                                <div className='flex flex-col'>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <Label htmlFor="nazwa">Nazwa</Label>
+                      <Input onChange={(e)=>handleInputChange("nazwa", e)} type="text" id="nazwa" placeholder="nazwa" />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <Label htmlFor="cena">Cena</Label>
+                      <Input onChange={(e)=>handleInputChange("cena", e)} type="text" id="cena" placeholder="cena" />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <Label htmlFor="opis">Opis</Label>
+                      <Input onChange={(e)=>handleInputChange("opis", e)} type="number" id="opis" placeholder="opis" />
+                    </div>
+                    <div className="grid w-full max-w-sm items-center gap-1.5">
+                      <Label htmlFor="zdj">Zdjecie</Label>
+                      <Input onChange={(e)=>{handleZdj(e)}} type="file" id="zdj" placeholder="zdj" />
+                    </div>
+                    <div className='flex flex-wrap gap-5 justify-center mt-5'>
+                    <Button onClick={handleSubmit}>Dodaj</Button> 
+                    
+                    </div>
+                </div>
 
-            ))
+                </div>
+}
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
 
-            }
         </div>
     )
 }
